39ddcd684f9cb898ec73f6954f6e37a1de9dc741
Brian Hamil <hamil.brian@gmail.com>
2019-09-17 17:10:31 -0500

    working buttons

--- MatchGame/MatchGame/GameViewController.swift
+++ MatchGame/MatchGame/GameViewController.swift
@@ -14,22 +14,39 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+// This code was from the base game kit supplied by Xcode, may not need
+//        if let view = self.view as! SKView? {
+//            // Load the SKScene from 'GameScene.sks'
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//                // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//
+//                // Present the scene
+//                view.presentScene(scene)
+//            }
+//
+//            view.ignoresSiblingOrder = true
+//
+//            view.showsFPS = true
+//            view.showsNodeCount = true
+//        }
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let PlayButton = UIButton()
+        let OptionsButton = UIButton()
+        
+        PlayButton.frame = CGRect(x: self.view.center.x - CGFloat(50), y: self.view.center.y - CGFloat(25), width: CGFloat(100), height: CGFloat(50))
+        PlayButton.backgroundColor = UIColor.red
+        PlayButton.setTitle("Play", for: .normal)
+        PlayButton.addTarget(self, action: #selector(playPressed), for: .touchUpInside)
+        self.view.addSubview(PlayButton)
+        
+        OptionsButton.frame = CGRect(x: PlayButton.center.x - CGFloat(50), y: PlayButton.center.y + CGFloat(50), width: CGFloat(100), height: CGFloat(50))
+        OptionsButton.backgroundColor = UIColor.blue
+        OptionsButton.setTitle("Options", for: .normal)
+        OptionsButton.addTarget(self, action: #selector(optionsPressed), for: .touchUpInside)
+        self.view.addSubview(OptionsButton)
+        
     }
 
     override var shouldAutorotate: Bool {
@@ -46,5 +63,13 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    @objc func playPressed() {
+        print("Play")
+    }
+    
+    @objc func optionsPressed() {
+        print("Options")
     }
 }
