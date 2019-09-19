bf0fa7932cc3a1a59d90d0cbd93deb2b57aaec08
Brian Hamil <hamil.brian@gmail.com>
2019-09-19 16:01:41 -0500

    changes

---
+++ MatchGame/MatchGame/MainViewController.swift
@@ -0,0 +1,63 @@
+//
+//  GameViewController.swift
+//  MatchGame
+//
+//  Created by Hamil, Brian (UMSL-Student) on 9/17/19.
+//  Copyright © 2019 Hamil, Brian (UMSL-Student). All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        showMainMenu()
+        
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func showMainMenu() {
+        
+        let PlayButton = UIButton()
+        let OptionsButton = UIButton()
+        
+        PlayButton.frame = CGRect(x: self.view.center.x - CGFloat(50), y: self.view.center.y - CGFloat(25), width: CGFloat(100), height: CGFloat(50))
+        PlayButton.backgroundColor = UIColor.red
+        PlayButton.setTitle("Play", for: .normal)
+        PlayButton.addTarget(self, action: #selector(playPressed), for: .touchUpInside)
+        self.view.addSubview(PlayButton)
+        
+        OptionsButton.frame = CGRect(x: PlayButton.center.x - CGFloat(50), y: PlayButton.center.y + CGFloat(50), width: CGFloat(100), height: CGFloat(50))
+        OptionsButton.backgroundColor = UIColor.blue
+        OptionsButton.setTitle("Options", for: .normal)
+        OptionsButton.addTarget(self, action: #selector(optionsPressed), for: .touchUpInside)
+        self.view.addSubview(OptionsButton)
+    }
+    
+    @objc func playPressed() {
+        let optionsMenu = OptionsViewController()
+        performSegue(withIdentifier: "OptionsView", sender: self)
+    }
+    
+    @objc func optionsPressed() {
+        print("Options")
+    }
+
+}
---
+++ MatchGame/MatchGame/OptionsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OptionsViewController.swift
+//  MatchGame
+//
+//  Created by Hamil, Brian (UMSL-Student) on 9/19/19.
+//  Copyright © 2019 Hamil, Brian (UMSL-Student). All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+    
+}
