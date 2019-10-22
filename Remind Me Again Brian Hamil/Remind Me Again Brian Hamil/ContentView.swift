8450afb7edc5efbf2a1070c8599b3d8ea7dce628
hamilbrian <31553972+hamilbrian@users.noreply.github.com>
2019-10-22 11:12:07 -0500

    Initial Commit

---
+++ Remind Me Again Brian Hamil/Remind Me Again Brian Hamil/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Remind Me Again Brian Hamil
+//
+//  Created by user159467 on 10/22/19.
+//  Copyright © 2019 user159467. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
